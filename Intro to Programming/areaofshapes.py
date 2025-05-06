312042cfe4e0fc71d0cbfffcb3ee7cb69359e0eb
davidav5000E <YOUR_EMAIL>
2025-05-06 17:29:16 -0600

    stuff

---
+++ Intro to Programming/areaofshapes.py
@@ -0,0 +1,15 @@
+lengthsq =input("What is the lenght of the side of the square? ")
+lenghtsq_input = int(lengthsq)
+areasquare = lenghtsq_input **2
+print(f"The area of the square is: {areasquare}") 
+lengthrec =input("\nWhat is the lenght of the of the rectangle? ")
+widthrec =input("\nWhat is the width the of the rectangle? ")
+widthrec_input = int(widthrec)
+lengthrec_input = int(lengthrec)
+arearec = lengthrec_input * widthrec_input
+print(f"\nThe area of the rectangle is: {arearec}")
+radius =input("What is the radius of the circle? ")
+radius_input = int(radius)
+areacir = radius_input **2 * 3.14
+print(f"\nThe area of the circle is: {areacir}")
+
---
+++ Intro to Programming/numericdatatypes.py
@@ -0,0 +1,16 @@
+age = input("How old are you? ")
+age_input = int(age)
+one = 1
+nbirthday = age_input + one
+print (f"\nOn your next birthday you will be {nbirthday}!")
+eggs = input("How many egg cartons do you have? ")
+eggs_input =int(eggs)
+twelve = 12
+cartons = eggs_input * twelve
+print(f"\nYou have {cartons} eggs.")
+cookies = input("\nHow many cookies do you have? ")
+people = input("\nHow many people are there? ")
+cookies_input = int(cookies)
+people_input = int(people)
+cookietotal= cookies_input / people_input
+print(f"\nEach person may have {cookietotal} cookies.")
