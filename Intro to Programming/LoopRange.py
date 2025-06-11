0d87da4cb6ed81612af436e020ff0a06e2a359ff
davidav5000E <YOUR_EMAIL>
2025-06-11 10:21:10 -0600

    1

---
+++ Intro to Programming/LoopRange.py
@@ -0,0 +1,13 @@
+colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    print(color)
+
+# Using range to iterate over a sequence of numbers
+print("")
+for i in range(1, 9):
+    print(i)
+
+# Using range with a step value
+print("")
+for i in range(2, 21, 2):
+    print(i)
