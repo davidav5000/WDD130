7be9c88d2961810c3730be4bea25570e53914b6f
davidav5000E <YOUR_EMAIL>
2025-06-18 12:36:24 -0600

    1

--- Intro to Programming/wordlegameBETA.py
+++ Intro to Programming/wordlegameBETA.py
@@ -32,16 +32,35 @@
 # print("\nThank you for using the program!")
 
 import random
-print("Welcome to the Wordle game!\n")
-bank = ["david","dan"]
+bank = ["david", "nephi", "jacob", "moroni", "alma", "helaman", "ammon", "lehi", "sam", "zoram", "liahona", "zion"]
 word = random.choice(bank)
 word_count = range(len(word))
+print("Welcome to the Wordle game!\n")
 print("Your hint is:", end=" ")
 for i, letter in enumerate(word):
     print("_", end="")
-guess = input("\nWhat is your guess? ")  
-guess_input = range(len(guess))
-if len(guess) != len(word):
-    print("Your guess is not the same length as the word.")
-else:
-    print("Your guess is:", end=" ")
+guess_number = 0
+guess = input("\nWhat is your guess? ").lower()
+guess_number += 1
+while guess != word:
+    if len(guess) != len(word):
+        print("Sorry, The guess must have the same number of letters as the secret word.")
+    else:
+        correct_letters = []
+        for i, letter in enumerate(guess):
+            if letter == word[i]:
+                #Capitalizes the first letter of the word
+                if i == 0:
+                    correct_letters.append(letter.upper())
+                else:
+                    correct_letters.append(letter.lower())
+            elif letter in word:
+                correct_letters.append(letter.lower())
+            else:
+                correct_letters.append("_")
+        print(" ".join(correct_letters))
+    guess = input("What is your guess? ").lower()
+    guess_number += 1
+if guess == word:
+    print("\nCongratulations! You guessed it:", word.capitalize())
+    print(f"\nIt took you {guess_number} guesses!")
